c7329ed7384a7ca118a3afdcfab0bb61c88c0206
enricopp <enricopietrop@gmail.com>
2022-06-01 20:43:28 -0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,18 @@ class MarimbaApp extends StatelessWidget {
     player.play('nota$numerosom.wav');
   }
 
+  Expanded criarBotao({cor, numeroDaNota}) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () {
+          tocarsom(numeroDaNota);
+        },
+        style: TextButton.styleFrom(backgroundColor: cor),
+        child: null,
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -18,70 +30,13 @@ class MarimbaApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                child: ElevatedButton(
-                  onPressed: () {
-                    tocarsom(1);
-                  },
-                  style: TextButton.styleFrom(backgroundColor: Colors.green),
-                  child: null,
-                ),
-              ),
-              Expanded(
-                child: ElevatedButton(
-                  onPressed: () {
-                    tocarsom(2);
-                  },
-                  style: TextButton.styleFrom(backgroundColor: Colors.blue),
-                  child: null,
-                ),
-              ),
-              Expanded(
-                child: ElevatedButton(
-                  onPressed: () {
-                    tocarsom(3);
-                  },
-                  style: TextButton.styleFrom(backgroundColor: Colors.yellow),
-                  child: null,
-                ),
-              ),
-              Expanded(
-                child: ElevatedButton(
-                  onPressed: () {
-                    tocarsom(4);
-                  },
-                  style: TextButton.styleFrom(backgroundColor: Colors.red),
-                  child: null,
-                ),
-              ),
-              Expanded(
-                child: ElevatedButton(
-                  onPressed: () {
-                    tocarsom(5);
-                  },
-                  style: TextButton.styleFrom(backgroundColor: Colors.grey),
-                  child: null,
-                ),
-              ),
-              Expanded(
-                child: ElevatedButton(
-                  onPressed: () {
-                    tocarsom(6);
-                  },
-                  style: TextButton.styleFrom(
-                      backgroundColor: Colors.purpleAccent),
-                  child: null,
-                ),
-              ),
-              Expanded(
-                child: ElevatedButton(
-                  onPressed: () {
-                    tocarsom(7);
-                  },
-                  style: TextButton.styleFrom(backgroundColor: Colors.brown),
-                  child: null,
-                ),
-              ),
+              criarBotao(cor: Colors.teal, numeroDaNota: 1),
+              criarBotao(cor: Colors.blue, numeroDaNota: 2),
+              criarBotao(cor: Colors.yellow, numeroDaNota: 3),
+              criarBotao(cor: Colors.red, numeroDaNota: 4),
+              criarBotao(cor: Colors.grey, numeroDaNota: 5),
+              criarBotao(cor: Colors.pink, numeroDaNota: 6),
+              criarBotao(cor: Colors.brown, numeroDaNota: 7),
             ],
           ),
         ),
